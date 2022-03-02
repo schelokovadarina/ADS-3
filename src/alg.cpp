f653aa767ea1386a0f651ca7f2f56feee23a0b2d
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-03-02 20:03:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,13 @@
 // Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
-int count = 0, int i = 0, j = size - 1;
+int cbinsearch(int* arr, int size, int value) {
+int count = 0, i = 0, j = size - 1;
 int mid = i + (j - i) / 2;
 for (int i = 0; i < mid; i++) {
 if (arr[i] == value)
 count++;
 }
 for (int j = size; j >= mid; j--) {
-f (arr[j] == value)
+if (arr[j] == value)
 count++;
 }
 return count;
